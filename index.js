7fdc5f46a556d34737c8cfa838fbb3a3d22fbe95
huqiangqiang <huqiangqiang@xiyun.com.cn>
2019-10-12 10:34:47 +0800

    fix: 接口参数修改

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ let noticeUrl = 'http://pic25.nipic.com/20121112/9252150_150552938000_2.jpg';
 let noticeTitle = '公告栏';
 
 
-const getNoticeData = (post) => {
-    post('/v1/list').then(() => {});
-    return 1
+const getNoticeData = (post, api) => {
+    post(api).then(() => {});
+    return 2
 }
 
 
@@ -16,12 +16,13 @@ const getNoticeData = (post) => {
 const isShowNotice = () => {
     oDiv.innerHTML = noticeTitle;
     oDiv.className = 'oDiv';
-    container[0].appendChild(oDiv);
+    container[0].parentNode.insertBefore(oDiv, container[0]);
 }
 
 // 显示图片式儿的公告
 const imgShowNotice = () => {
     oImg.src = noticeUrl;
+    oImg.className = 'oImg'
     container[0].appendChild(oImg);
 }
 
